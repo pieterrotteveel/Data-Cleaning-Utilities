7e90bf401954497ef4bd46382cce01f564e655fb
Pieter Christiaan Rotteveel <96623649+pietertherat@users.noreply.github.com>
2025-09-27 17:27:42 +0200

    Remove docstring from to_long function
    Removed docstring from to_long function.

---
+++ to_long.py
@@ -0,0 +1,8 @@
+def to_long(
+    df: pd.DataFrame,
+    id_vars: Optional[Sequence[str]] = None,
+    value_vars: Optional[Sequence[str]] = None,
+    var_name: str = "variable",
+    value_name: str = "value",
+) -> pd.DataFrame:
+    return pd.melt(df, id_vars=id_vars, value_vars=value_vars, var_name=var_name, value_name=value_name)
